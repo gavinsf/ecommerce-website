f087ae2c12d98c67c55932404df1ae5c5048ca9b
gavinsf <gsfoster031@gmail.com>
2026-06-09 02:43:50 -0700

    Adds basic models

---
+++ app/models.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, String, Float, Integer, ForeignKey, Enum, DateTime
+from app.database import Base
+from sqlalchemy.dialects.postgresql import UUID
+import uuid
+import enum
+import datetime
+
+
+class OrderStatus(enum.Enum):
+    pending   = "pending"
+    paid      = "paid"
+    shipped   = "shipped"
+    cancelled = "cancelled"
+
+class User(Base):
+    __tablename__ = "users"
+    id            = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    email         = Column(String, primary_key=True, nullable=False)
+    hash          = Column(String, nullable=False)
+    created_at    = Column(DateTime, default=datetime.now)
+
+class Product(Base):
+    __tablename__ = "products"
+    id            = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+class Order(Base):
+    __tablename__ = "orders"
+    id            = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    user_id       = Column(UUID(as_uuid=True), ForeignKey("users.id"))
+    status        = Column(Enum(OrderStatus), default=OrderStatus.pending)
+    total         = Column(Float)
+    created_at    = Column(DateTime, default=datetime.now)
+
+class OrderItem(Base):
+    __tablename__ = "order_items"
+    id            = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    order_id      = Column(UUID(as_uuid=True), ForeignKey("orders.id"))
+    product_id    = Column(UUID(as_uuid=True), ForeignKey("products.id"))
+    quantity      = Column(Integer)
+    unit_price    = Column(Float)

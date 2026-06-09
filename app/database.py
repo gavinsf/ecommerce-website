1e87583065ad254d0503738b4faea179a0d9ced5
gavinsf <gsfoster031@gmail.com>
2026-06-09 02:19:07 -0700

    Initial commit

---
+++ app/database.py
@@ -0,0 +1,18 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
+from app.config import settings
+
+
+engine = create_async_engine(
+    settings.DB_URL,
+    pool_size = 10,
+    max_overflow = 20,
+    pool_pre_ping = True,
+    echo=settings.DEBUG
+)
+
+SessionLocal = async_sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False,
+    autoflush=False
+)

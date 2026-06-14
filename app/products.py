468b08a8ddafd00ee043598860a45b2f1e46a866
gavinsf <gsfoster031@gmail.com>
2026-06-13 17:45:30 -0700

    Adds get_product endpoint

--- app/products.py
+++ app/products.py
@@ -9,16 +9,33 @@ router = APIRouter(
     tags=["products"])
 
 
+
+
+# TODO : Require admin
+@router.get("/{id}")
+async def get_product(id: UUID, db=Depends(get_db)):
+    product = await db.get(Product, id)
+
+    if not product:
+        return {"error" : "Product not found"}
+    
+    if product.is_deleted == 1:
+        return {"log" : "Product already deleted"}
+    
+    return product
+
+
 # TODO : Require admin
 @router.post("/")
 async def create_product(prod: ProductCreate, db=Depends(get_db)):
-    db_product = Product(**prod.model_dump())
-    db.add(db_product)
+    product = Product(**prod.model_dump())
+    db.add(product)
     await db.commit()
-    await db.refresh(db_product)
-    return db_product
+    await db.refresh(product)
+    return product
 
 
+# TODO : Require admin
 @router.delete("/{id}")
 async def delete_product(id: UUID, db=Depends(get_db)):
     product = await db.get(Product, id)
@@ -26,7 +43,7 @@ async def delete_product(id: UUID, db=Depends(get_db)):
     if not product:
         return {"error" : "Product not found"}
     
-    product.is_deleted = True
+    product.is_deleted = 1
 
     await db.commit()
 

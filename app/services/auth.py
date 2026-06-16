1e8b15ebf2d1d30aa8cf550e2181c49ffa872fae
gavinsf <gsfoster031@gmail.com>
2026-06-15 18:19:32 -0700

    Adds auth services

---
+++ app/services/auth.py
@@ -0,0 +1,36 @@
+from jose import JWTError, jwt
+import bcrypt
+from app.config import settings
+import uuid
+from datetime import datetime, UTC, timedelta
+from fastapi import HTTPException
+
+
+def hash_pwd(password: str):
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt()).decode()
+
+def verify_pwd(plain: str, hashed: str):
+    return bcrypt.checkpw(plain.encode(), hashed.encode())
+
+def create_access_token(user_id: uuid.UUID, groups: list[str]):
+    claims = {
+        "sub"    : str(user_id),
+        "groups" : groups,
+        "type"   : "access",
+        "exp"    : datetime.now(UTC) + timedelta(seconds=settings.ACCESS_TOKEN_EXPIRE)
+    }
+    return jwt.encode(claims, settings.JWT_SECRET, algorithm="HS256")
+
+def create_refresh_token(user_id: uuid.UUID):
+    claims = {
+        "sub"   : str(user_id),
+        "type"  : "refresh",
+        "exp"   : datetime.now(UTC) + timedelta(seconds=settings.REFRESH_TOKEN_EXPIRE)
+    }
+    return jwt.encode(claims, settings.JWT_SECRET, algorithm="HS256")
+
+def decode_token(token: str):
+    try:
+        return jwt.decode(token, settings.JWT_SECRET, algorithms=["HS256"])
+    except JWTError:
+        raise HTTPException(401, "Invalid or expired token")

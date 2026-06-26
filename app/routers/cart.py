8d5acd76c2b7c41fb8baedb910de7efffbf3f1b4
gavinsf <gsfoster031@gmail.com>
2026-06-25 19:06:10 -0700

    Adds ability to add item to cart

---
+++ app/routers/cart.py
@@ -0,0 +1,45 @@
+from app.schemas.cart import CartItemResponse, CartResponse, CartItemAdd
+from fastapi import APIRouter, Depends, HTTPException
+from app.dependencies import get_db, get_current_user
+from app.models import Product, CartItem
+from sqlalchemy import select
+
+router = APIRouter(
+    prefix="/cart",
+    tags=["cart"])
+
+@router.post("/items", response_model=CartItemResponse)
+async def add_item_cart(payload: CartItemAdd,
+    db=Depends(get_db), user=Depends(get_current_user)):
+    sel = select(Product).where(Product.id == payload.product_id)
+    res = await db.execute(sel)
+    prod = res.scalars().first()
+
+    if not prod:
+        raise HTTPException(status_code=404, detail="Product cannot be found")
+    
+    if prod.is_deleted == 1:
+        raise HTTPException(status_code=403, detail="Product is deleted")
+    
+    sel = select(CartItem).where(
+        CartItem.user_id == user.id,
+        CartItem.product_id == payload.product_id
+    )
+    res = await db.execute(sel)
+    existing = res.scalars().first()
+
+    if existing:
+        existing.quantity += payload.quantity
+        item = existing
+    else:
+        item = CartItem(id = user.id, product_id = payload.product_id, quantity = payload.quantity)
+        db.add(item)
+    await db.commit()
+    await db.refresh(item)
+
+    return CartItemResponse(
+        product_id = payload.id,
+        quantity = payload.quantity,
+        price = prod.sell_price * payload.quantity,
+        name = prod.name
+    )

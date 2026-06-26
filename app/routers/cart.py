f47a70e93567390e7c3e10ddb9057c85cef1cc44
gavinsf <gsfoster031@gmail.com>
2026-06-25 19:32:01 -0700

    Adds clear_cart feature

--- app/routers/cart.py
+++ app/routers/cart.py
@@ -83,3 +83,18 @@ async def update_item_cart(id, payload=CartItemUpdate, db=Depends(get_db), user=
         price = round(prod.sell_price * payload.quantity, 2),
         name = prod.name
     )
+
+@router.delete("/")
+async def clear_cart(db=Depends(get_db), user=Depends(get_current_user)):
+    sel = select(CartItem).where(CartItem.user_id==user.id)
+    res = await db.execute(sel)
+    items = res.scalars().all()
+
+    if not items:
+        raise HTTPException(status_code=404, detail="Cart not found")
+    
+    for item in items:
+        db.delete(item)
+    await db.commit()
+
+    return {"Detail" : "Cart cleared"}

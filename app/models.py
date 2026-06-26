accfcb3f42c9c28c70886e8cb85e2f9437546718
gavinsf <gsfoster031@gmail.com>
2026-06-25 19:06:44 -0700

    Updates CartItem model to include product_id

--- app/models.py
+++ app/models.py
@@ -49,6 +49,7 @@ class CartItem(Base):
     __tablename__ = "carts"
     id            = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     user_id       = Column(UUID(as_uuid=True), ForeignKey("users.id"), nullable=False)
+    product_id    = Column(UUID(as_uuid=True), ForeignKey("products.id"), nullable=False)
     quantity      = Column(Integer, nullable=False, default=1)
     created_at    = Column(DateTime, default=datetime.datetime.now)
     

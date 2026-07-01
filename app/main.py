0515bb85e642fe325118563f95dab0ca6344d178
gavinsf <gsfoster031@gmail.com>
2026-06-30 22:15:55 -0700

    Adds basic main

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI
-from app.products import router as products_router
+from app.routers.auth import router as auth_router
+from app.routers.cart import router as cart_router
+from app.routers.products import router as products_router
+from app.routers.orders import router as orders_router
 
 app = FastAPI()
 
 app.include_router(products_router, prefix="/api")
+app.include_router(cart_router, prefix="/api")
+app.include_router(auth_router, prefix="/api")
+app.include_router(orders_router, prefix="/api")
 
 @app.get("/")
 async def root():
